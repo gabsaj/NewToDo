6975bb4248a20ef3a73b5e8daa53898d02dc7594
Gabriel Šajfar <gabriel.sajfar@gmail.com>
2022-11-10 23:08:43 +0000

    base for task

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 
 function App() {
-  return <></>;
+  const [toDoList, setToDoList] = useState<string[]>(["task1", "task2"]);
+  const [toDoTask, setToDoTask] = useState<string>("");
+
+  const handleSubmit = () => {};
+  const handleEdit = () => {};
+
+  return (
+    <>
+      <div className="todo">
+        <div className="todo--add">
+          <input onChange={(e) => setToDoTask(e.target.value)} type="text" />
+          <button>Add task</button>
+          <button>Edit</button>
+        </div>
+        <ul className="todo--list">
+          {toDoList.map((item) => (
+            <li>
+              {item}
+              <button>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
